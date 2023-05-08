2764980d7bd317a307ba1ab04058a416620bfdda
Alexis Beauregard <mariposa_lex99@yahoo.com>
2023-05-07 23:50:24 -0400

    Shows a draft of thought

---
+++ index.js
@@ -0,0 +1,12 @@
+const btnPee = document.getElementById('btn-pee');
+const btnPoop = document.getElementById('btn-poop');
+
+// Add click event listeners to each button
+btnPee.addEventListener('click', () => {
+    console.log('Pee clicked');
+  });
+
+btnPoop.addEventListener('click', () => {
+    console.log('Poop clicked');
+  });
+  
