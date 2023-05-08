3f93402714fcbe750f63a203a4d0b135da2eaace
Alexis Beauregard <mariposa_lex99@yahoo.com>
2023-05-08 00:07:57 -0400

    Adds additional event listeners

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const btnPee = document.getElementById('btn-pee');
 const btnPoop = document.getElementById('btn-poop');
+const btnMixed = document.getElementById('btn-mixed');
+const btnDry = document.getElementById('btn-dry');
 
 // Add click event listeners to each button
 btnPee.addEventListener('click', () => {
@@ -8,5 +10,11 @@ btnPee.addEventListener('click', () => {
 
 btnPoop.addEventListener('click', () => {
     console.log('Poop clicked');
+  });
+  btnMixed.addEventListener('click', () => {
+    console.log('Mixed clicked');
+  });
+  btnDry.addEventListener('click', () => {
+console.log('Dry clicked')
   });
   
