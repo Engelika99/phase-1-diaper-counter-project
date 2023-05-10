606fd05b8cb74730199c862a78a2f586dd2cd92f
Alexis Beauregard <mariposa_lex99@yahoo.com>
2023-05-10 15:32:08 -0400

    modifies variables to fit web app needs

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
+//DOM elements
 document.addEventListener("DOMContentLoaded", function() {
-const btnPee = document.getElementById('btn-pee');
-const btnPoop = document.getElementById('btn-poop');
-const btnMixed = document.getElementById('btn-mixed');
-const btnDry = document.getElementById('btn-dry');
-
-// Add click event listeners to each button
-btnPee.addEventListener('click', () => {
-    console.log('Pee clicked');
-  });
-
-btnPoop.addEventListener('click', () => {
-    console.log('Poop clicked');
-  });
-  btnMixed.addEventListener('click', () => {
-    console.log('Mixed clicked');
-  });
-  btnDry.addEventListener('click', () => {
-console.log('Dry clicked')
-  });
+const addWetBtn = document.getElementById('add-wet');
+const addPoopBtn = document.getElementById('add-poop');
+const resetBtn = document.getElementById('reset');
+const wetCounts = document.getElementById('wet-count');
+const poopCounts = document.getElementById('poop-count'); 
 });
