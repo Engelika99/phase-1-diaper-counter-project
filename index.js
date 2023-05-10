dd50ee4a2e0732ab03360a57b697936ce20266ee
Alexis Beauregard <mariposa_lex99@yahoo.com>
2023-05-10 16:13:57 -0400

    Adds event listeners for clicks

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const addPoopBtn = document.getElementById('add-poop');
 const resetBtn = document.getElementById('reset');
 const wetCounts = document.getElementById('wet-count');
 const poopCounts = document.getElementById('poop-count'); 
-});
+
 //Initialize counts
 let wetCount = 0;
 let poopCount = 0;
@@ -14,3 +14,20 @@ function updateCounts() {
     wetCounts.textContent = wetCount;
     poopCounts.textContent = poopCount;
 }
+// Event Listener for the add wet button
+addWetBtn.addEventListener('click', () => {
+wetCount++;
+updateCounts();
+});
+//Event listener for the add poop button
+addPoopBtn.addEventListener('click', () => {
+    poopCount++;
+    updateCounts();
+});
+//Event lisinter for the reset button
+resetBtn.addEventListener('click', () => {
+    wetCount = 0;
+    poopCount = 0;
+    updateCounts();
+});
+});
